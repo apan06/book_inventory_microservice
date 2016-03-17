b7a2ff518320534e8927072f069e068959576846
Michał Apanowicz <michal.apanowicz@schibsted.pl>
2016-03-17 11:16:02 +0100

    refactor infra

--- Infrastructure/prod.js
+++ Infrastructure/prod.js
@@ -1,38 +1,21 @@
 /**
  * Created by michalapanowicz on 17/03/16.
  */
+var _=require('lodash');
+var baseConfig = require('./base').config;
+var configurator = require('./base').configurator;
 
-var heroin = require('heroin-js');
 
-var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+var prod = {
+    name: 'mapano-book-inventory-service',
+    config_vars: {
+        MONGOLAB_URI: process.env.MONGOLAB_URI,
+        NAME: "Michał"
+    },
+    addons: {mongolab: {plan: 'mongolab:sandbox'}},
+    domains: ['mapano-book-inventory-service.herokuapp.com']
+};
 
-configurator.export('mapano-book-inventory-service').then(function (result) {
-    console.log(result);
-});
 
-configurator(
-    {
-        name: 'mapano-book-inventory-service',
-        region: 'eu',
-        maintenance: false,
-        stack: 'cedar-14',
-        config_vars: {
-            MONGOLAB_URI: process.env.MONGOLAB_URI,
-            NAME: "Michał"
-        },
-        addons: {mongolab: {plan: 'mongolab:sandbox'}},
-        collaborators: ['michal.apanowicz@schibsted.pl',
-            'wojciech.sromek@schibsted.pl'],
-        features: {
-            'runtime-dyno-metadata': {enabled: false},
-            'log-runtime-metrics': {enabled: false},
-            'http-session-affinity': {enabled: false},
-            preboot: {enabled: false},
-            'http-shard-header': {enabled: false},
-            'http-end-to-end-continue': {enabled: false}
-        },
-        formation: [{process: 'web', quantity: 1, size: 'Free'}],
-        log_drains: [],
-        domains: ['mapano-book-inventory-service.herokuapp.com']
-    }
-);
+var finalConfig = _.merge({}, baseConfig, prod);
+configurator(finalConfig);
---
+++ Infrastructure/test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by michalapanowicz on 17/03/16.
+ */
+
+var _=require('lodash');
+var baseConfig = require('./base').config;
+var configurator = require('.base').configurator;
+
+var test = {
+    name: 'mapano-book-inventory-test',
+    config_vars: {
+        NAME: "Michał-test"
+    },
+    addons: {mongolab: {plan: 'mongolab:sandbox'}}
+};
+
+var finalConfig = _.merge({}, baseConfig, test);
+configurator(finalConfig);
