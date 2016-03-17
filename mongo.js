d2d4ed6f48db0036d56650f7a116cf5c635741ac
Michał Apanowicz <michal.apanowicz@schibsted.pl>
2016-03-17 10:17:27 +0100

    env paths

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/myproject';
+var url = process.evn.MONGOLAB_URI || 'mongodb://localhost:27017/myproject';
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -26,15 +26,6 @@ var insertDocuments = function(db, callback) {
         isbn: isbn,
         count: count
     }, {upsert: true});
-    //collection.insertMany([
-    //    {a : 1}, {a : 2}, {a : 3}
-    //], function(err, result) {
-    //    assert.equal(err, null);
-    //    assert.equal(3, result.result.n);
-    //    assert.equal(3, result.ops.length);
-    //    console.log("Inserted 3 documents into the document collection");
-    //    callback(result);
-    //});
 };
 
 
